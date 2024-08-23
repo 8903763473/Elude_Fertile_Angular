4fe8210a8e238e296b378f722ad0c34ecdb20dfd
Vijay <vijayakumar050301@gmail.com>
2024-08-23 13:57:25 +0530

    Initial Commit by vijay

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor() {}
+  constructor() { }
+
+  menuItems = [
+    { id: 1, src: '../assets/menu/Home.svg', alt: 'Home' },
+    { id: 2, src: '../assets/menu/Discount.svg', alt: 'Discount' },
+    { id: 3, src: '../assets/menu/Graph.svg', alt: 'Graph' },
+    { id: 4, src: '../assets/menu/Message.svg', alt: 'Message' },
+    { id: 5, src: '../assets/menu/Notification.svg', alt: 'Notification' },
+    { id: 6, src: '../assets/menu/Setting.svg', alt: 'Setting' },
+    { id: 7, src: '../assets/menu/Logout.svg', alt: 'Logout' }
+  ];
+
+  selectedMenu(data: any, id: any) {
+
+  }
 }
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,6 +7,61 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  tableStatus: any = [
+    {
+      id: 1,
+      color: '#0AC511',
+      name: 'Blank Table'
+    },
+    {
+      id: 2,
+      color: '#7B89FF',
+      name: 'Running Table'
+    },
+    {
+      id: 3,
+      color: '#AF6868',
+      name: 'Printed Table'
+    },
+    {
+      id: 4,
+      color: '#FF7E20',
+      name: 'Paid Table'
+    },
+    {
+      id: 5,
+      color: '#8022A1',
+      name: 'Running KOT Table'
+    }
+  ]
+
+  dummy: any = [
+    {
+      id: 1
+    },
+    {
+      id: 1
+    },
+    {
+      id: 1
+    },
+    {
+      id: 1
+    },
+    {
+      id: 1
+    },
+    {
+      id: 1
+    },
+    {
+      id: 1
+    },
+    {
+      id: 1
+    }
+  ]
+
+  constructor() { }
 
 }
